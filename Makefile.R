1ea456d208fb87745d815b9f3df055633b2b22b3
jennybc <jenny@stat.ubc.ca>
2013-12-06 17:34:11 -0800

    Make the makefile fancier so we don't re-run data gathering and cleaning very often

--- Makefile.R
+++ Makefile.R
@@ -1,12 +1,22 @@
-
-
+## fine-tune the cleaning strategy to avoid this redoing this step in most
+## scenarios; set nuke to TRUE if you want to re-clean and assemble the data
+nuke <- FALSE
 
 ## clean out any previous work
-outputs <- c("lfsdat.csv",               # the clean data
-             "WageTrendRegression.csv",  # The Table built from the second script 
-             list.files(pattern = "*.png$"))
+if(nuke) {
+  outputs <- c("lfsdat.csv",               # the clean data
+               "WageTrendRegression.csv",  # The Table built from the second script 
+               list.files(pattern = "*.png$"))
+} else {
+  outputs <- list.files(pattern = "*.png$")
+}
+## clean out previous work
 file.remove(outputs)
 
 ## run my two scripts from the beginning
-source("01_preprocessData.R")   ## Reads the big dataset and makes the required changes and saves the clean data 
-source("02_analysisPlots.R")    ## analyse global terrorism using plots
+if(!file.exists("lfsdat.csv")) {
+  ## Reads the big dataset and makes the required changes and saves the clean
+  ## data
+  source("01_preprocessData.R")
+}
+source("02_analysisPlots.R")    ## analyse and plot labor force data
